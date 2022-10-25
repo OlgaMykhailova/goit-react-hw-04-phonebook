4ced7a82b2a67cff905fe1054a0afb8e6afd36e0
OlgaMykhailova <rosebud_z@yahoo.com>
2022-10-25 22:10:48 +0200

    made refactoring App+ContactForm

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,81 +1,69 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { Notify } from 'notiflix';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+export const App = () => {
+const defaultValue = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+]
 
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(localStorage.getItem('contacts')) ?? defaultValue;
+  });
+  const [filter, setFilter] = useState('');
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
+ 
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-  addContact = newContact => {
-    const contactList = this.state.contacts;
+  const addContact = newContact => {
     const newContactNameNormalized = newContact.name.toLowerCase();
 
-    const findContact = contactList.find(
+    const findContact = contacts.find(
       contact => contact.name.toLowerCase() === newContactNameNormalized
     );
 
     findContact
       ? Notify.warning(`${newContact.name} is already in contacts`)
-      : this.setState(({ contacts }) => ({
-          contacts: [newContact, ...contacts],
-        }));
+      : setContacts(contacts => [newContact, ...contacts]);
   };
 
-  deleteContact = contactId => {
-    this.setState(({ contacts }) => ({
-      contacts: contacts.filter(contact => contact.id !== contactId),
-    }));
+  const deleteContact = contactId => {
+    setContacts(contacts =>
+      contacts.filter(contact => contact.id !== contactId)
+    );
   };
 
-  changeFilter = e => {
+  const changeFilter = e => {
     const { value } = e.currentTarget;
-    this.setState({ filter: value });
+    setFilter(value);
   };
 
-  getVisibleContacts = () => {
-    const contactList = this.state.contacts;
-    const normalizedFilter = this.state.filter.toLowerCase();
+  const getVisibleContacts = () => {
+    // const contactList = this.state.contacts;
+    const normalizedFilter = filter.toLowerCase();
 
-    return contactList.filter(contact =>
+    return contacts.filter(contact =>
       contact.name.toLowerCase().includes(normalizedFilter)
     );
   };
 
-  render() {
-    return (
-      <div className="container">
-        <h1>Phonebook</h1>
-        <ContactForm onSubmit={this.addContact} />
-        <h2>Contacts</h2>
-        <Filter value={this.state.filter} onFilter={this.changeFilter} />
-        <ContactList
-          visibleContacts={this.getVisibleContacts()}
-          onDeleteContact={this.deleteContact}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="container">
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={addContact} />
+      <h2>Contacts</h2>
+      <Filter value={filter} onFilter={changeFilter} />
+      <ContactList
+        visibleContacts={getVisibleContacts()}
+        onDeleteContact={deleteContact}
+      />
+    </div>
+  );
+};
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,51 +1,55 @@
 import { Formik } from 'formik';
 import * as yup from 'yup';
 import { nanoid } from 'nanoid';
-import { Component } from 'react';
-import { Form, Label, Text, Field, Button, ErrorMessage } from './ContactFormStyled';
+import {
+  Form,
+  Label,
+  Text,
+  Field,
+  Button,
+  ErrorMessage,
+} from './ContactFormStyled';
 
 const schema = yup.object().shape({
-    name: yup.string().required(),
-    number: yup.number().positive().integer().required(),
-  });
+  name: yup.string().required(),
+  number: yup.number().positive().integer().required(),
+});
 
-export class ContactForm extends Component {
-  state = {
+export const ContactForm = ({ onSubmit }) => {
+  const initialValues = {
     name: '',
     number: '',
   };
 
-  handleSubmit = (values, actions) => {
+  const handleSubmit = (values, actions) => {
     const newContact = {
       id: nanoid(6),
       name: values.name,
       number: values.number,
     };
-    this.props.onSubmit(newContact);
+    onSubmit(newContact);
     actions.resetForm();
   };
 
-  render() {
-    return (
-      <Formik
-        initialValues={this.state}
-        onSubmit={this.handleSubmit}
-        validationSchema={schema}
-      >
-        <Form>
-          <Label>
-            <Text>Name</Text>
-            <Field type="text" name="name"></Field>
-            <ErrorMessage name="name" component="span"></ErrorMessage>
-          </Label>
-          <Label>
-            <Text>Number</Text>
-            <Field type="tel" name="number"></Field>
-            <ErrorMessage name="number" component="span"></ErrorMessage>
-          </Label>
-          <Button type="submit">Add contact</Button>
-        </Form>
-      </Formik>
-    );
-  }
-}
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validationSchema={schema}
+    >
+      <Form>
+        <Label>
+          <Text>Name</Text>
+          <Field type="text" name="name"></Field>
+          <ErrorMessage name="name" component="span"></ErrorMessage>
+        </Label>
+        <Label>
+          <Text>Number</Text>
+          <Field type="tel" name="number"></Field>
+          <ErrorMessage name="number" component="span"></ErrorMessage>
+        </Label>
+        <Button type="submit">Add contact</Button>
+      </Form>
+    </Formik>
+  );
+};
